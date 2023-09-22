84a7e5e49e1d42c2ad53f0824ba19073fb8c7f8c
Georgi <sgeorgi174@gmail.com>
2023-09-22 17:21:28 +0500

    the react app was been created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1 className='app__title'>Здесь скоро будет Skypro.Music</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
