64ba040b5208a05e5f5e62e97831b5152378a03f
Georgi <sgeorgi174@gmail.com>
2023-09-24 20:41:43 +0500

    burger button was moved to a separate component

---
+++ src/components/NavBurgerButton/NavBurgerButton.jsx
@@ -0,0 +1,11 @@
+function NavBurgerButton() {
+  return (
+    <div className="nav__burger burger">
+      <span className="burger__line" />
+      <span className="burger__line" />
+      <span className="burger__line" />
+    </div>
+  )
+}
+
+export default NavBurgerButton
--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -2,6 +2,7 @@ import './NavMenu.css'
 import NavHomePageButton from '../NavHomePageButton/NavHomePageButton'
 import NavMyPlaylistButton from '../NavMyPlaylistButton/NavMyPlaylistButton'
 import NavSighInButton from '../NavSighInButton/NavSighInButton'
+import NavBurgerButton from '../NavBurgerButton/NavBurgerButton'
 
 function NavMenu() {
   return (
@@ -9,11 +10,7 @@ function NavMenu() {
       <div className="nav__logo logo">
         <img className="logo__image" src="img/logo.png" alt="logo" />
       </div>
-      <div className="nav__burger burger">
-        <span className="burger__line" />
-        <span className="burger__line" />
-        <span className="burger__line" />
-      </div>
+      <NavBurgerButton />
       <div className="nav__menu menu">
         <ul className="menu__list">
           <NavHomePageButton />
